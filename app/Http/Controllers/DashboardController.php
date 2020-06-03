4491889376050eec24a520e5ea94dbebc665de2d
David Noguera Gutierrez <dnoguera@imagar.com>
2020-06-03 16:53:49 +0200

    Updater

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -22,7 +22,7 @@ class DashboardController extends Controller
             ->get();
 
         $update_app = false;
-        //dd($updater->source());
+        //dd($updater->source()->getVersionInstalled());
         // Check if new version is available
         if($updater->source()->isNewVersionAvailable()) {
             $update_app = true;
